0d3f00ab3ec4b32a650d50408017378ae4f4adc9
RandhawaSushant <99862731+RandhawaSushant@users.noreply.github.com>
2022-11-28 18:19:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+!pip install -q streamlit
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+
+st.write("""
+Sum of two numbers
+""")
+
+st.header('User Input')
+
+def user_input_features():
+  number_1=st.number_input("First_number",min_value=None,max_value=None)
+  number_2=st.number_input("Second_number",min_value=None,max_value=None)
+  data = {'First number': number_1,
+      'Second number':number_2}
+  features=pd.DataFrame(data,index=[0])
+  return features
+
+df=user_input_features()
+
+sum=df.add
+
+st.write(sum)
